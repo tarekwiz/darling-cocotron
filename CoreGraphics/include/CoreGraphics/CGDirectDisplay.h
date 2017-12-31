131ed3ac597cb5b0cdcf005a344a9784e7faa933
Andrew Hyatt <ahyattdev@icloud.com>
2017-12-30 19:11:19 -0800

    Fix CoreGraphics compilation

--- CoreGraphics/include/CoreGraphics/CGDirectDisplay.h
+++ CoreGraphics/include/CoreGraphics/CGDirectDisplay.h
@@ -2,4 +2,8 @@
 #import <CoreGraphics/CoreGraphicsExport.h>
 #import <CoreGraphics/CGError.h>
 
+typedef uint32_t CGDirectDisplayID;
+typedef uint32_t CGOpenGLDisplayMask;
+typedef double CGRefreshRate;
+
 COREGRAPHICS_EXPORT CGError CGReleaseAllDisplays(void);
