28bbebe9bfe3a029e10bbd8cb9d9413077d9caeb
Andrew Hyatt <ahyattdev@icloud.com>
2018-07-26 00:51:25 -0400

    Define CGFloat in the proper file

---
+++ CoreGraphics/include/CoreGraphics/CGBase.h
@@ -0,0 +1,21 @@
+#ifndef __CGBase_H__
+#define __CGBase_H__
+
+#include <float.h>
+
+#if defined(__LP64__) && __LP64__
+# define CGFLOAT_TYPE double
+# define CGFLOAT_IS_DOUBLE 1
+# define CGFLOAT_MIN DBL_MIN
+# define CGFLOAT_MAX DBL_MAX
+#else
+# define CGFLOAT_TYPE float
+# define CGFLOAT_IS_DOUBLE 0
+# define CGFLOAT_MIN FLT_MIN
+# define CGFLOAT_MAX FLT_MAX
+#endif
+
+typedef CGFLOAT_TYPE CGFloat;
+#define CGFLOAT_DEFINED 1
+
+#endif
