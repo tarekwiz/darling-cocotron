76b2fdef2904f6f6b5a6a9ac644fc0c45ed29035
Andrew Hyatt <ahyattdev@icloud.com>
2018-07-26 00:57:06 -0400

    Include CF

--- CoreGraphics/include/CoreGraphics/CGFont.h
+++ CoreGraphics/include/CoreGraphics/CGFont.h
@@ -9,6 +9,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #import <CoreGraphics/CoreGraphicsExport.h>
 #import <CoreGraphics/CGGeometry.h>
 
+#include <CoreFoundation/CoreFoundation.h>
+
 typedef struct O2Font *CGFontRef;
 
 typedef uint16_t CGGlyph;
