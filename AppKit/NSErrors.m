95d4f03cccc8ccdc341eb8ec69fbcef7a2faa547
Lubos Dolezel <lubos@dolezel.info>
2018-08-14 14:44:48 +0200

    Add NSErrors definitions

---
+++ AppKit/NSErrors.m
@@ -0,0 +1,40 @@
+#import "NSErrors.h"
+
+NSExceptionName NSTextLineTooLongException = @"NSTextLineTooLongException";
+NSExceptionName NSTextNoSelectionException = @"NSTextNoSelectionException";
+NSExceptionName NSWordTablesWriteException = @"NSWordTablesWriteException";
+NSExceptionName NSWordTablesReadException = @"NSWordTablesReadException";
+NSExceptionName NSTextReadException = @"NSTextReadException";
+NSExceptionName NSTextWriteException = @"NSTextWriteException";
+NSExceptionName NSPasteboardCommunicationException = @"NSPasteboardCommunicationException";
+NSExceptionName NSPrintingCommunicationException = @"NSPrintingCommunicationException";
+NSExceptionName NSAbortModalException = @"NSAbortModalException";
+NSExceptionName NSAbortPrintingException = @"NSAbortPrintingException";
+NSExceptionName NSIllegalSelectorException = @"NSIllegalSelectorException";
+NSExceptionName NSAppKitVirtualMemoryException = @"NSAppKitVirtualMemoryException";
+NSExceptionName NSBadRTFDirectiveException = @"NSBadRTFDirectiveException";
+NSExceptionName NSBadRTFFontTableException = @"NSBadRTFFontTableException";
+NSExceptionName NSBadRTFStyleSheetException = @"NSBadRTFStyleSheetException";
+NSExceptionName NSTypedStreamVersionException = @"NSTypedStreamVersionException";
+NSExceptionName NSTIFFException = @"NSTIFFException";
+NSExceptionName NSPrintPackageException = @"NSPrintPackageException";
+NSExceptionName NSBadRTFColorTableException = @"NSBadRTFColorTableException";
+NSExceptionName NSDraggingException = @"NSDraggingException";
+NSExceptionName NSColorListIOException = @"NSColorListIOException";
+NSExceptionName NSColorListNotEditableException = @"NSColorListNotEditableException";
+NSExceptionName NSBadBitmapParametersException = @"NSBadBitmapParametersException";
+NSExceptionName NSWindowServerCommunicationException = @"NSWindowServerCommunicationException";
+NSExceptionName NSFontUnavailableException = @"NSFontUnavailableException";
+NSExceptionName NSPPDIncludeNotFoundException = @"NSPPDIncludeNotFoundException";
+NSExceptionName NSPPDParseException = @"NSPPDParseException";
+NSExceptionName NSPPDIncludeStackOverflowException = @"NSPPDIncludeStackOverflowException";
+NSExceptionName NSPPDIncludeStackUnderflowException = @"NSPPDIncludeStackUnderflowException";
+NSExceptionName NSRTFPropertyStackOverflowException = @"NSRTFPropertyStackOverflowException";
+NSExceptionName NSAppKitIgnoredException = @"NSAppKitIgnoredException";
+NSExceptionName NSBadComparisonException = @"NSBadComparisonException";
+NSExceptionName NSImageCacheException = @"NSImageCacheException";
+NSExceptionName NSNibLoadingException = @"NSNibLoadingException";
+NSExceptionName NSBrowserIllegalDelegateException = @"NSBrowserIllegalDelegateException";
+NSExceptionName NSAccessibilityException = @"NSAccessibilityException";
+
+
