269215f67029441269482261b27289d6f961bd76
Lubos Dolezel <lubos@dolezel.info>
2018-09-04 16:12:44 +0200

    Build fix

--- CoreGraphics/include/CoreGraphics/CGFont.h
+++ CoreGraphics/include/CoreGraphics/CGFont.h
@@ -10,6 +10,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #import <CoreGraphics/CGGeometry.h>
 
 #include <CoreFoundation/CoreFoundation.h>
+#import <Foundation/NSObjCRuntime.h>
 
 typedef struct O2Font *CGFontRef;
 
